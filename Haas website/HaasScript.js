de28be26472a2cc6bd56d83a898d861b2a91479b
Thomas <thomaschapman128@gmail.com>
2025-05-21 16:07:37 +0100

    Add files via upload

---
+++ Haas website/HaasScript.js
@@ -0,0 +1,77 @@
+
+function displayDriver(driverName) {
+	fetch("https://raw.githubusercontent.com/tom-chapman-5/driver_data/refs/heads/main/Haas_drivers.json")
+		.then(response => {
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+			return response.json();
+		})
+		.then(data => {
+			const driver = data.find(d => d.name === driverName);
+
+			if (driver) {
+				const driverCard = document.createElement('div');
+				driverCard.classList.add('driver-card');
+
+				driverCard.innerHTML = `
+					<h2>${driver.name}</h2>
+					<img src="${driver.pic}" alt="${driver.name}" width="20%">
+					
+					<h3>Stats</h3>
+					<p>Total Seasons: ${driver.seasons}</p>
+					<p>Total Wins: ${driver.wins}</p>
+					<p>Total Poles: ${driver.poles}</p>
+					<p>Total Races: ${driver.races}</p>
+					
+					<h3>Stats with Haas</h3>
+					<p>Seasons: ${driver.seasons_haas}</p>
+					<p>Wins: ${driver.wins_haas}</p>
+					<p>Poles: ${driver.poles_haas}</p>
+					<p>Races: ${driver.races_haas}</p>
+					
+					<h3>Information</h3>
+					<p>Description:</p>
+					<p>${driver.description}</p>
+					<p>Career:</p>
+					<p>${driver.career}</p>
+				`;
+				
+				box.appendChild(driverCard);
+			}
+		})
+}
+
+function deleteInfo(driverName) {
+	const driverInfoDiv = document.getElementById("box");
+        driverInfoDiv.innerHTML = '';
+	displayDriver(driverName);
+}
+
+function displayStats() {
+	const driverInfoDiv = document.getElementById("box");
+        driverInfoDiv.innerHTML = '';
+	const driverCard = document.createElement('div');
+		driverCard.classList.add('driver-card');
+	driverCard.innerHTML = `
+		<h2>Stats</h2>
+				<ul>
+			<li style="float: none;">WDC: 0</li>
+			<li style="float: none;">WCC: 0</li>
+			<li style="float: none;">Race Wins: 0</li>
+			<li style="float: none;">Podiums: 0</li>
+			<li style="float: none;">Poles: 1</li>
+			<li style="float: none;">Races: 196</li>
+		</ul>
+		<h2>2025 Stats</h2>
+		<ul>
+			<li style="float: none;">Race Wins: 0</li>
+			<li style="float: none;">Poles: 0</li>
+			<li style="float: none;">Strint Wins: 0</li>
+			<li style="float: none;">Points: 20</li>
+			<li style="float: none;">Podiums: 0</li>
+		</ul>
+
+	`;
+	box.appendChild(driverCard);
+}
